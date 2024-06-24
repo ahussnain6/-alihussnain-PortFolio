655e9200cb653711f2eb3e576c4c598df4e0c4d7
Ali Hussnain <143886682+ahussnain6@users.noreply.github.com>
2024-06-24 21:59:07 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
                 <li><a href="#about">About</a></li>
                 <li><a href="#skill">Skills</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a href="https://drive.google.com/file/d/1PIfJR-kMRl-UOz9CNSfbMWwbN_2Sew1Z/view?usp=drivesdk " file target='_blank' download>Resume</a></li>
+                <li><a href="https://drive.google.com/file/d/1eBgh4RDzyxqBKOCY81HMEMxw9iGnyLS2/view?usp=drivesdk" file target='_blank' download>Resume</a></li>
             </ul>
             <h1 className="pl" ><a href="#" className="pli" style={{textDecoration:"none"}}>Ali Hussnain</a></h1>
         </div>
